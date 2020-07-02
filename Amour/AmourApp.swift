5cf4217677fa0db802f30ff0fb8f668832b2a73c
Stephen Zyszkiewicz <stevez@me.com>
2020-07-01 20:00:26 -0500

    Amour initial commit

---
+++ Amour/AmourApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmourApp.swift
+//  Amour
+//
+//  Created by steve on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct AmourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Amour/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Amour
+//
+//  Created by steve on 6/26/20.
+//
+
+import SwiftUI
+import AuthenticationServices
+
+// sign in with Apple
+//func onRequest(request: ASAuthorizationAppleIDRequest) {
+//
+//}
+//
+//func onCompletion(result: Result<ASAuthorization, Error>) {
+//
+//}
+
+struct ContentView: View {
+    var body: some View {
+            TabView {
+                Text("Checkout")
+                    .font(.title)
+                    .tabItem {
+                        HStack {
+                            Image(systemName: "heart.fill")
+                            Text("Checkout")
+                        }
+                    }
+                Text("Likes")
+                    .font(.title)
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "hand.thumbsup.fill")
+                            Text("Likes")
+                        }
+                    }
+                Text("Conversations")
+                    .font(.title)
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "message.fill")
+                            Text("Conversations")
+                        }
+                    }
+                Text("Profile")
+                    .font(.title)
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "person.crop.circle.fill")
+                            Text("Profile")
+                        }
+                    }
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 8")
+    }
+}
